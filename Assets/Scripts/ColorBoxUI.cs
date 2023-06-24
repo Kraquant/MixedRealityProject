76768be7fa8d99a0bc2419d739d264ba9f5592d8
luis <luis.martinez.sanchez@gmail.com>
2023-06-24 09:48:36 +0900

    Color saving working. Missing visual effects so you can see when the box is selected.

--- Assets/Scripts/ColorBoxUI.cs
+++ Assets/Scripts/ColorBoxUI.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ColorBoxUI : MonoBehaviour
+public class ColorBoxUI : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private Image colorBoxImage;
+    private bool isBoxSelected = false;
 
     public void UpdateColor(Color color)
     {
         colorBoxImage.color = color;
+    }
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        isBoxSelected = !isBoxSelected;
+    }
+    
+    public bool GetIsBoxSelected()
+    {
+    	return isBoxSelected;
     }
 }
